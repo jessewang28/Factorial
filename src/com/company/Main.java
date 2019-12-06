72f53adff0f59f75cbc92af55f7fd9fb428fccda
student <jessewang@billericak12.com>
2019-12-06 11:17:30 -0500

    Commit'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,43 @@ package com.company;
 
 public class Main {
 
-    public static void calcFactorial(int n){
-        int sum=1;
+    public static long calcFactorial(int n){
+        long sum=1;
         for (int k=n; k>0; k--){
             sum=sum*k;
         }
-        System.out.println(sum);
+        return sum;
+    }
+
+    public static double calcE(){
+        int counter=1;
+        double e=1;
+        double olde=0;
+        while(e-olde > .001){
+            olde=e;
+            e=olde + 1.0/calcFactorial(counter);
+            counter++;
+            }
+        return e;
+    }
+
+    public static double calceX(){
+        
     }
 
     public static void main(String[] args) {
 	    for (int j=1; j<=10;j++){
-	        calcFactorial(j);
+	        long a = calcFactorial(j);
+	        System.out.println(a);
         }
 	    System.out.println();
         for (int j=1; j<=20;j++){
-            calcFactorial(j);
+            long b = calcFactorial(j);
+            System.out.println(b);
         }
+        double e = calcE();
+        System.out.printf("e is %2.3f \n", e);
+
     }
 }
+
