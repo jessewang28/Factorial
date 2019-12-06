3b3347e196098b69af6d856b56810ae931932b72
student <jessewang@billericak12.com>
2019-12-06 11:23:49 -0500

    Commit'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int counter=1;
         double e=1;
         double olde=0;
-        while(e-olde > .001){
+        while(e-olde > .0001){
             olde=e;
             e=olde + 1.0/calcFactorial(counter);
             counter++;
@@ -22,8 +22,16 @@ public class Main {
         return e;
     }
 
-    public static double calceX(){
-        
+    public static double calcEX(int x){
+        int counter=1;
+        double e=1;
+        double olde=0;
+        while(e-olde > .0001){
+            olde=e;
+            e=olde + Math.pow(x, counter)/calcFactorial(counter);
+            counter++;
+        }
+        return e;
     }
 
     public static void main(String[] args) {
@@ -36,8 +44,15 @@ public class Main {
             long b = calcFactorial(j);
             System.out.println(b);
         }
+
         double e = calcE();
         System.out.printf("e is %2.3f \n", e);
+
+        for (int x = 1; x<=5; x++){
+            double c = calcEX(x);
+            System.out.printf("e is %2.3f \n", c);
+        }
+
 
     }
 }
